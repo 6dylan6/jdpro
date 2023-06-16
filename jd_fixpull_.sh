4afbaeae14c16d4f6b911a17ead5342262462375
dylan <58234511@qq.com>
2023-06-16 13:36:06 +0800

    1

--- jd_fixpull_.sh
+++ jd_fixpull_.sh
@@ -14,8 +14,8 @@ function fix(){
         	    rm -rf /ql/data/repo/6dylan6_jdpro
 				#ql repo https://ghproxy.com/https://github.com/6dylan6/jdpro.git "jd_|jx_|jddj_" "backUp" "^jd[^_]|USER|JD|function|sendNotify"
             else
-                echo -e '修复失败！\n将下面目录结构截图提问' 
-                find /ql -maxdepth 2 -type d
+                echo -e '无需修复，拉不动可能是代理问题' 
+                #find /ql -maxdepth 2 -type d
                 return 1
             fi
         else
@@ -26,8 +26,8 @@ function fix(){
         	    rm -rf /ql/data/repo/6dylan6_jdpro_main
 				#ql repo https://ghproxy.com/https://github.com/6dylan6/jdpro.git "jd_|jx_|jddj_" "backUp" "^jd[^_]|USER|JD|function|sendNotify" "main"
             else
-                echo -e '修复失败！将下面目录结构截图提问\n'
-                find /ql -maxdepth 2 -type d
+                echo -e '无需修复，拉不动可能是代理问题\n'
+                #find /ql -maxdepth 2 -type d
                 return 1
             fi
         fi
@@ -35,5 +35,5 @@ function fix(){
 
 fix
 
-[[ $(echo $?) -eq 0 ]]  && echo -e '修复完成，再拉库试试！，如果还不行是网络问题了！！！'
+[[ $(echo $?) -eq 0 ]]  && echo -e '修复完成，再拉库试试！，如果还不行是网络或代理问题！！！'
 	
