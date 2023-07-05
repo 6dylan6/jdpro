c3ad8b9f173071622ed9a52713ecc36ecdbbd8ce
dylan <58234511@qq.com>
2023-07-05 20:45:16 +0800

    1

--- jd_bean_change.js
+++ jd_bean_change.js
@@ -2019,11 +2019,11 @@ function GetDateTime(date) {
 async function queryScores() {
     let res = ''
     let url = {
-      url: `https://rsp.jd.com/windControl/queryScore/v1?lt=m&an=plus.mobile&stamp=${Date.now()}`,
+      url: `https://api.m.jd.com/api?functionId=windControl_queryScore_v1&appid=plus_business&loginType=2&loginWQBiz=plus&scval=&body=%7B%7D`,
       headers: {
         'Cookie': cookie,
         'User-Agent': 'Mozilla/5.0 (Linux; Android 10; Redmi Note 8 Pro Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045715 Mobile Safari/537.36',
-        'Referer': 'https://plus.m.jd.com/rights/windControl'
+        'Referer': 'https://plus.m.jd.com/user/home'
       }
     };
 	
