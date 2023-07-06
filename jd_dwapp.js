49579e6c98e721fa2f7b442d6061457719919348
dylan <58234511@qq.com>
2023-07-06 19:32:58 +0800

    1

--- jd_dwapp.js
+++ jd_dwapp.js
@@ -42,7 +42,7 @@ if ($.isNode()) {
             }
             $.UUID = getUUID('xxxxxxxxxxxxxxxx');
             await main();
-            await $.wait(3000);
+            await $.wait(5000);
         }
     }
 })().catch((e) => { $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '') }).finally(() => { $.done(); })
