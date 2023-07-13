a1699d19db77f5813c5d64fbc6ba53ead8ccf7d2
dylan <58234511@qq.com>
2023-07-13 22:50:05 +0800

    1

--- function/sendNotify.js
+++ function/sendNotify.js
@@ -21,7 +21,7 @@ const querystring = require('querystring');
 const exec = require('child_process').exec;
 const $ = new Env();
 const timeout = 15000; //超时时间(单位毫秒)
-console.log("加载sendNotify，当前版本: 20230314");
+console.log("加载sendNotify，当前版本: 20230712");
 // =======================================go-cqhttp通知设置区域===========================================
 //gobot_url 填写请求地址http://127.0.0.1/send_private_msg
 //gobot_token 填写在go-cqhttp文件设置的访问密钥
